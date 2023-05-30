c950847e4a1df48538fa994bd92ea352094d553c
tamibyte <dev@tamerhayek.com>
2023-05-30 17:55:36 +0200

    badge with metacritic added

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+	score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+	let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+	return (
+		<Badge
+			colorScheme={color}
+			fontSize={"14px"}
+			paddingX={2}
+			borderRadius={"10px"}
+		>
+			{score}
+		</Badge>
+	);
+};
+
+export default CriticScore;
+
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
 	game: Game;
@@ -9,12 +10,19 @@ interface Props {
 const GameCard = ({ game }: Props) => {
 	return (
 		<Card borderRadius={10} overflow='hidden'>
-			<Image src={game.background_image} alt={game.name} />
-			<CardBody>
+			<Image
+				src={game.background_image}
+				alt={game.name}
+				aspectRatio={16 / 9}
+			/>
+			<CardBody marginY={3}>
 				<Heading fontSize='2xl'>{game.name}</Heading>
-				<PlatformIconList
-					platforms={game.parent_platforms.map((p) => p.platform)}
-				/>
+				<HStack mt={2} justifyContent={"space-between"}>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
 			</CardBody>
 		</Card>
 	);
