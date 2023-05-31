4d2cda808e3ab61c32cdcbeeae7ff550acb23283
tamibyte <dev@tamerhayek.com>
2023-05-31 14:58:38 +0200

    skeleton for fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,18 +1,37 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+	HStack,
+	Image,
+	List,
+	ListItem,
+	Skeleton,
+	SkeletonText,
+	Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/imageUrl";
 
 const GenreList = () => {
 	const { data, isLoading, error } = useGenres();
+	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-	if (error) {
-		return <Text>{error}</Text>;
+	if (error) return null;
+
+	if (isLoading) {
+		return (
+			<List>
+				{skeletons.map((skeleton) => (
+					<ListItem key={skeleton} paddingY={"5px"}>
+						<Skeleton height={"32px"} />
+					</ListItem>
+				))}
+			</List>
+		);
 	}
 
 	return (
 		<List>
 			{data.map((genre) => (
-				<ListItem key={genre.id} paddingY={"5px"} >
+				<ListItem key={genre.id} paddingY={"5px"}>
 					<HStack>
 						<Image
 							src={getCroppedImageUrl(genre.image_background)}
