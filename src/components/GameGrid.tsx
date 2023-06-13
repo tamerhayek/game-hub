a53c10359f1adcf0f34020424ed120df75a854a5
tamerhayek <dev@tamerhayek.com>
2023-06-13 18:42:06 +0200

    columns fixes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,11 @@ const GameGrid = ({ gameQuery }: Props) => {
 	}
 
 	return (
-		<SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3} paddingX={3}>
+		<SimpleGrid
+			columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+			spacing={3}
+			paddingX={3}
+		>
 			{isLoading &&
 				skeletons.map((skeleton) => (
 					<GameCardContainer key={skeleton}>
