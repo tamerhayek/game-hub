34db2bd0f88333f0bee2e61d7bfaddebe692508f
tamerhayek <dev@tamerhayek.com>
2023-06-30 14:12:54 +0200

    added search games responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,28 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image, Show } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const Navbar = () => {
 	return (
-		<HStack justifyContent="space-between" padding="10px" spacing={10}>
-			<Image
-				src={logo}
-				alt="Logo"
-				boxSize="60px"
-				cursor={"pointer"}
-				onClick={() => window.location.reload()}
-			/>
-			<SearchInput />
-			<ColorModeSwitch />
-		</HStack>
+		<Box padding={"10px"}>
+			<HStack justifyContent="space-between" spacing={10} paddingY={5}>
+				<Image
+					src={logo}
+					alt="Logo"
+					boxSize="60px"
+					cursor={"pointer"}
+					onClick={() => window.location.reload()}
+				/>
+				<Show above="lg">
+					<SearchInput />
+				</Show>
+				<ColorModeSwitch />
+			</HStack>
+			<Show below="lg">
+				<SearchInput />
+			</Show>
+		</Box>
 	);
 };
 
