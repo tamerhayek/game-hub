7872dfdba0568da38f7601543a9731c2d652381a
tamerhayek <dev@tamerhayek.com>
2023-06-30 17:22:58 +0200

    build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
-import { Box, Heading, Spinner } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -15,6 +18,7 @@ const GameDetailPage = () => {
 		<Box padding={5}>
 			<Heading marginBottom={3}>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
+			<GameAttributes game={game} />
 		</Box>
 	);
 };
