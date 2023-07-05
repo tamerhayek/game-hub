0d7663296e45a331f06c17da09bcd65877e5ef77
tamerhayek <dev@tamerhayek.com>
2023-07-05 14:19:39 +0200

    grid added to game page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,10 +1,10 @@
-import { Box, Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameScreenshots from "../components/GameScreenshots";
 import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
-import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -15,13 +15,17 @@ const GameDetailPage = () => {
 	if (error || !game) throw error;
 
 	return (
-		<Box padding={5}>
-			<Heading marginBottom={3}>{game.name}</Heading>
-			<ExpandableText>{game.description}</ExpandableText>
-			<GameAttributes game={game} />
-			<GameTrailer gameId={game.id} />
-			<GameScreenshots gameId={game.id} />
-		</Box>
+		<SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} padding={5}>
+			<GridItem>
+				<Heading marginBottom={3}>{game.name}</Heading>
+				<ExpandableText>{game.description}</ExpandableText>
+				<GameAttributes game={game} />
+			</GridItem>
+			<GridItem>
+				<GameTrailer gameId={game.id} />
+				<GameScreenshots gameId={game.id} />
+			</GridItem>
+		</SimpleGrid>
 	);
 };
 
